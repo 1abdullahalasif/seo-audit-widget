ee0faa4968b745be1a6e2050e0def667567ae1bb
Abdullah Al Asif <100970719+1abdullahalasif@users.noreply.github.com>
2026-04-15 03:04:12 +1200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,23 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  env: {
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
-  },
-  async headers() {
-    return [
-      {
-        source: '/:path*',
-        headers: [
-          {
-            key: 'X-Frame-Options',
-            value: 'ALLOWALL'
-          }
-        ],
-      },
-    ];
-  }
-};
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
